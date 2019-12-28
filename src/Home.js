059e34494fcbc1daf71f957a2b49b593a248c9eb
Ale Bausa <ale.bausa@gmail.com>
2019-12-28 23:50:16 +0100

    WIP home page

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,37 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <div>
-        This is the home page
+      <div className="container">
+        <header className="header">
+          <div className="col-2">
+            Image
+          </div>
+          <div className="col-2">
+            <h1>Fullstack developer &amp; <br />webapp designer</h1>
+            <h2>Bringing your ideas to life</h2>
+            <button className="primary">Calculate the price of your project for free ></button>
+          </div>
+        </header>
+        <section className="body-section">
+          <div className="col-2">
+            <h3>Remarkable projects</h3>
+            <ul className="icon-list">
+              <li>www.iconfessapp.com</li>
+              <li>www.sigridfadrique.com</li>
+              <li>www.lamodebcn.com</li>
+              <li>www.elartesano.com</li>
+              <li>Misc.</li>
+            </ul>
+          </div>
+          <div className="col-2">
+            Images
+          </div>
+        </section>
+        <section className="body-section">
+          <div className="col-1">
+
+          </div>
+        </section>
       </div>
     );
   }
